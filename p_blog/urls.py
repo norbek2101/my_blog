dd08855d4cd48c441e9f9aba30f793ece9ae72c6
norbek <lightnight1223@gmail.com>
2021-09-10 11:14:04 +0500

    Applar yaratildi, Class Based View qo'llanildi, Login, Logout, Register qismlar qilindi

---
+++ p_blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    # path('', home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(template_name='blog/post_detail.html'), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(template_name='blog/post_form.html'), name='post-create'),
+    # path('', PostListView.as_view(), name='blog-home'),
+    # path('', PostListView.as_view(), name='blog-home'),
+    path('about/', about, name='blog-about'),
+]
---
+++ p_blog/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+
+from .models import Post
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all(),
+        'title': 'Home'
+    }
+    return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+def about(request):
+    context = {
+        'title': 'About'
+    }
+    return render(request, 'blog/about.html', context)
