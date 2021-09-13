ffd35241b340036935b9c71456ee5d8c2c174380
norbek <lightnight1223@gmail.com>
2021-09-13 13:38:56 +0500

    Paginator funksiyasi qo'shildi

--- p_blog/urls.py
+++ p_blog/urls.py
@@ -5,9 +5,12 @@ from .views import *
 urlpatterns = [
     # path('', home, name='blog-home'),
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>/', UserPostListView.as_view(template_name='blog/user_posts.html'), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(template_name='blog/post_detail.html'), name='post-detail'),
     path('post/new/', PostCreateView.as_view(template_name='blog/post_form.html'), name='post-create'),
     # path('', PostListView.as_view(), name='blog-home'),
     # path('', PostListView.as_view(), name='blog-home'),
     path('about/', about, name='blog-about'),
+
+    path('gotopage/', gotopage, name='gotopage')
 ]
--- p_blog/views.py
+++ p_blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Post
@@ -12,11 +13,27 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 
+def gotopage(request):
+    page = request.GET['page']
+    return redirect('/blog/?page=' + str(page))
+
+
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 6
+
+
+class UserPostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
